e85c1b1467e28430f8f1db2c4e1628ad67cba585
unknown <felipe.silvad@outlook.com>
2024-01-31 01:53:41 -0300

    partner skill

--- src/PalTemp/PalCard/PalCardTabs.js
+++ src/PalTemp/PalCard/PalCardTabs.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Image } from 'react-bootstrap';
 import PalCardTabsStats from './PalCardTabsStats';
 import PalCardTabsWork from './PalCardTabsWork';
+import PalCardTabsPSkill from './PalCardTabsPSkill';
+
 function PalCardTabs({pal, cardTab, setCardTab}) {
 
   if (pal) {
@@ -11,15 +13,19 @@ function PalCardTabs({pal, cardTab, setCardTab}) {
           <div className={`tab-select-option ${(cardTab===1)&&('active')}`} onClick={() => setCardTab(1)}>Stats</div>
           <div className={`tab-select-option ${(cardTab===2)&&('active')}`} onClick={() => setCardTab(2)}>Speed</div>
           <div className={`tab-select-option ${(cardTab===3)&&('active')}`} onClick={() => setCardTab(3)}>Work</div>
+          <div className={`tab-select-option ${(cardTab===4)&&('active')}`} onClick={() => setCardTab(4)}>Skill</div>
         </div>
         {(cardTab===1)&&(
-          <PalCardTabsStats stats={pal.main_stats} />
-        )}
+          <PalCardTabsWork work={pal.work} />
+          )}
         {(cardTab===2)&&(
+          <PalCardTabsStats stats={pal.main_stats} />
+          )}
+        {(cardTab===3)&&(
           <PalCardTabsStats stats={pal.speed} />
         )}
-        {(cardTab===3)&&(
-          <PalCardTabsWork work={pal.work} />
+        {(cardTab===4)&&(
+          <PalCardTabsPSkill skillID={pal.partner_skill} />
         )}
       </div>
     );
